cae446c9abdb57f678451bb7bd1f730ee4887c50
Tiago Santos Hucs <tiago.hucs@spread.com.br>
2024-07-28 15:41:38 -0300

    install bootstrap bootstrap icons + 2 components and routes

--- front-end/fake-bank/src/app/app.routes.ts
+++ front-end/fake-bank/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './external/sign-in/sign-in.component';
+import { SignUpComponent } from './external/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'signin', component: SignInComponent },
+    { path: 'signup', component: SignUpComponent },
+];
